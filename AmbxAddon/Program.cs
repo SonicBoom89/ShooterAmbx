c0da0bb7befa066a9192305011b06b1f35686f8d
SonicBoom <bergbauer.wolfgang@googlemail.com>
2015-01-26 23:59:40 +0100

    Init

---
+++ AmbxAddon/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmbxAddon
+{
+    class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AmbxAddon addon = new AmbxAddon();
+            Application.Run(addon);
+        }
+
+    }
+}
